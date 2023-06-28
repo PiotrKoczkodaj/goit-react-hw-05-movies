1d904ec9671377a3f9aa69dbb7994ad5f9ba1a57
PiotrKoczkodaj <piotr.koczkodaj@vp.pl>
2023-06-28 19:53:33 +0200

    .

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,17 +1,16 @@
-
-import { useParams } from "react-router-dom";
-import { fetchTrendingMovies } from "utils/FetchTrendingMovies";
+// import { useEffect,useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { fetchTrendingMovies } from "utils/FetchTrendingMovies";
 
 export const MovieDetailsPage = () => {
-    const { movieId } = useParams();
+    // const { movieId } = useParams();
  
 
-    fetchTrendingMovies().then(resp => {
-        resp.data.results.map(movie => {
-            console.log(movie.id===movieId)
+    // fetchTrendingMovies().then(resp => {
+    //     resp.data.results.map(movie => {
+    //         console.log(movie.id===movieId)
             
-        })
-    return null})
+    // })})
     return (
         <p>movieees</p>
     )
